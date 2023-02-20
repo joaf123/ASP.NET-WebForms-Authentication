f0df4094aaa8f5b9520b3aad885c0401680f438b
Joa <56757601+joaf123@users.noreply.github.com>
2023-02-20 10:28:23 +0100

    Update module with comments

--- AuthorizationHttpModule.cs
+++ AuthorizationHttpModule.cs
@@ -1,22 +1,39 @@
-﻿using System;
+using System;
 using System.Reflection;
 using System.Web;
 using System.Web.Compilation;
 using System.Web.Security;
 using System.Web.UI;
-
+/// <summary>
+/// Verify authorization on call to WebMethods or Pages with the attribute: &lt;RequiresAuthentication&gt; (VB) | [RequiresAuthentication] (C#).
+/// </summary>
 [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
 public class RequiresAuthenticationAttribute : Attribute { }
 
+/// <summary>
+/// Attribute based forms authentication verification module.
+/// </summary>
 public class AttributeBasedFormsAuthenticationModule : IHttpModule {
+    /// <summary>
+    /// Inits the AttributeBasedFormsAuthentication Module.
+    /// </summary>
+    /// <param name="application">HttpApplication Parameter</param>
     public void Init(HttpApplication application) {
         application.PostMapRequestHandler += OnPostAuthorizeRequest;
     }
 
+    /// <summary>
+    /// Disposes the AttributeBasedFormsAuthentication Module
+    /// </summary>
     public void Dispose() {
         // Clean up resources, if any
     }
 
+    /// <summary>
+    /// Authorize request.
+    /// </summary>
+    /// <param name="sender">Sender Parameter</param>
+    /// <param name="e">EventArgs Parameter</param>
     private void OnPostAuthorizeRequest(object sender, EventArgs e) {
         var app = (HttpApplication)sender;
         var context = app.Context;
@@ -43,12 +60,21 @@ public class AttributeBasedFormsAuthenticationModule : IHttpModule {
         }
     }
 
+    /// <summary>
+    /// Deny access.
+    /// </summary>
+    /// <param name="context">The context.</param>
     private static void DenyAccess(HttpContext context) {
         context.Response.StatusCode = 401;
         context.Response.SuppressContent = true;
         context.Response.End();
     }
 
+    /// <summary>
+    /// Gets the web method name from request.
+    /// </summary>
+    /// <param name="request">The request.</param>
+    /// <returns>WebMethod name as string.</returns>
     private static string GetWebMethodNameFromRequest(HttpRequest request) {
         var pathInfo = request.PathInfo.TrimStart('/');
         var slashIndex = pathInfo.IndexOf('/');
